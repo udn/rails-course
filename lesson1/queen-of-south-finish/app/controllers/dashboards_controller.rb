03d9c352f79df9afd23d7f84267ca410a2def569
burhanudin hakim <udnpico@gmail.com>
2017-08-11 16:50:46 +0700

    update

--- lesson1/queen-of-south-finish/app/controllers/dashboards_controller.rb
+++ lesson1/queen-of-south-finish/app/controllers/dashboards_controller.rb
@@ -1,7 +1,12 @@
 class DashboardsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @partner = current_user
-    @messages = @partner.enquiries
+    if current_user.type != "Partner" 
+      sign_out(current_user) 
+      redirect_to root_url, alert: 'Not Partner Account'
+    else
+      @partner = current_user
+      @messages = @partner.enquiries
+    end
   end
 end
--- lesson1/queen-of-south-finish/app/controllers/properties_controller.rb
+++ lesson1/queen-of-south-finish/app/controllers/properties_controller.rb
@@ -1,5 +1,5 @@
 class PropertiesController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update, :create, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :create, :new, :destroy]
   before_action :set_property, only: [:show, :edit, :update, :destroy]
 
   # GET /properties
